c6949e796f7d914a08dc915a418ed5468e4dc452
Kevin Garabedian <kevin.garabedian@signalwire.com>
2021-02-09 18:24:11 -0500

    Added CSharp Example Consumer

---
+++ csharp/PhoneConsumer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using SignalWire.Relay;
+using SignalWire.Relay.Calling;
+
+namespace ConsumerExample
+{
+    [Serializable]
+    internal class PhoneConsumer : Consumer
+    {        
+        protected override void Setup()
+        {
+
+        }
+
+        protected override void OnIncomingCall(Call call)
+        {
+            AnswerResult resultAnswer = call.Answer();
+            if (!resultAnswer.Successful)
+            {
+            }
+            else
+            {
+                // incoming call failed
+            }
+
+            call.PlayTTS("The quick brown fox jumps over the lazy dog.");
+            TerminateCall(call);
+        }
+
+        public void TerminateCall(Call call)
+        {
+            call.Hangup();
+        }
+    }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsumerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => {
+                var pc = new PhoneConsumer
+                {
+                    Project = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+                    Token = "PTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+
+                    Contexts = new System.Collections.Generic.List<string>() { "ExampleContext" }
+                };
+                pc.Run();
+     
+            });
+            Console.WriteLine("Running...");
+            Console.ReadKey();
+        }
+    }
+}
